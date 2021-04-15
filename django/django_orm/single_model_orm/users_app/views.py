a6cf83e4f2ddf2652851c30d31fc70b44604dec9
= <takashizono@gmail.com>
2021-04-14 23:18:17 -0500

    added sql_queries_orm, updated single_model_orm

--- django/django_orm/single_model_orm/users_app/views.py
+++ django/django_orm/single_model_orm/users_app/views.py
@@ -1,7 +1,19 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Users
 
 def index(request):
-    return HttpResponse("This is my response!")
+    users =  Users.objects.all()
+    return render(request, 'index.html', {'users': users})
+
+def add_user(request):
+    if request.method == 'POST':
+        Users.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            age = request.POST['age'],
+        )
+    return redirect('/')
