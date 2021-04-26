5ec93e384e911b9d05de54af232d464928b76e7e
= <takashizono@gmail.com>
2021-04-25 21:40:55 -0500

    modified Ninja Gold

--- django/django_intro/ninja_gold/app_ninja_gold/urls.py
+++ django/django_intro/ninja_gold/app_ninja_gold/urls.py
@@ -4,7 +4,4 @@ urlpatterns = [
     path('', views.main),
     path('process_money/<location>', views.process_money),
     path('reset', views.reset),
-    # path('process_money/cave', views.process_money_cave),
-    # path('process_money/house', views.process_money_house),
-    # path('process_money/casino', views.process_money_casino),
 ]
--- django/django_intro/ninja_gold/app_ninja_gold/views.py
+++ django/django_intro/ninja_gold/app_ninja_gold/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from pytz import timezone
 import random, pytz
 
-# Create your views here.
 def main(request):
     if 'counter' not in request.session:
         request.session['counter'] = 0
@@ -11,11 +10,19 @@ def main(request):
         request.session['gold'] = 0
     if 'transactions' not in request.session:
         request.session['transactions'] = []
-    if (request.session['gold'] >= 100):
-        request.session.flush()
-        return render(request, 'game_clear.html')
-
+    win = False
+    if request.session['gold'] >= 100:
+        win = True
+        return render(request, 'result.html', {'win': win})
+    if request.session['counter'] >= 7:
+        if request.session['gold'] >= 100:
+            win = True
+        return render(request, 'result.html', {'win': win})
     return render(request, 'main.html')
+
+def rand(min, max):
+    num = random.randint(min, max)
+    return num
 
 def process_money(request, location):
     dateTimeObj = datetime.now(tz=pytz.utc)
@@ -46,44 +53,4 @@ def process_money(request, location):
 def reset(request):
     request.session.flush()
     return redirect('/')
-
-def rand(min, max):
-    num = random.randint(min, max)
-    return num
-
-# def process_money_cave(request):
-#     if 'cave' not in request.session:
-#         request.session['cave'] = ""
-#     dateTimeObj = datetime.now()
-#     timestampStr = dateTimeObj.strftime("%Y/%m/%d (%H:%M:%S)")
-#     money = rand(5,10)
-#     request.session['cave'] += "Earned {} golds from the cave! ({}) / ".format(money, timestampStr)
-#     request.session['counter'] += 1
-#     request.session['gold'] += money
-#     return redirect('/')
-
-# def process_money_house(request):
-#     if 'house' not in request.session:
-#         request.session['house'] = ""
-#     dateTimeObj = datetime.now()
-#     timestampStr = dateTimeObj.strftime("%Y/%m/%d (%H:%M:%S)")
-#     money = rand(2,5)
-#     request.session['house'] += "Earned {} golds from the house! ({}) / ".format(money, timestampStr)
-#     request.session['counter'] += 1
-#     request.session['gold'] += money
-#     return redirect('/')
-
-# def process_money_casino(request):
-#     if 'casino' not in request.session:
-#         request.session['casino'] = ""
-#     dateTimeObj = datetime.now()
-#     timestampStr = dateTimeObj.strftime("%Y/%m/%d (%H:%M:%S)")
-#     money = rand(-50,50)
-#     if (money >= 0):
-#         request.session['casino'] += "Earned {} golds from the casino! ({}) / ".format(money, timestampStr)
-#     else:
-#         request.session['casino'] += "Lost {} golds at the casino! ({}) / ".format(money, timestampStr)
-#     request.session['counter'] += 1
-#     request.session['gold'] += money
-#     return redirect('/')
 
